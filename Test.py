8ce90cfd4ac3636103b862184d2e37e5c8ecf52d
HornedCoder <kayush5u2018@gmail.com>
2024-05-30 14:46:22 +0530

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,22 @@
+'''
+print('Fukumean')
+
+def f(value,values):
+    value = 1
+    values[0] = 44
+
+t = 3
+v = [1,2,3]
+
+f(t,v)
+print(t,v[0])
+'''
+stack = []
+stack.append([2])
+print(stack)
+stack.append([3])
+print(stack)
+stack[-1].append(1)
+print(stack)
+stack.append(0)
+print(stack)
